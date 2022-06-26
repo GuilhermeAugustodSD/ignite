3d72cc1e68b3f59aa7710456a60ad40ea97a2928
Guilherme Augusto <guilhermednts2@gmail.com>
2022-06-26 16:08:15 -0300

    Cadastro no GraphQL usando mutation

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
- uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oy1c4k1lt001w722pkep28/master',
+ uri: import.meta.env.VITE_API_URL,
+ headers: {
+  'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+ },
  cache: new InMemoryCache()
 })
